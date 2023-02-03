979a84374e42468bc87c3adeff8b4c4778c7ecbe
Sergio Pichardo <ing.sergio.pichardo.a@gmail.com>
2023-02-03 13:39:03 -0600

    Se declararon todos los valores de nuentra calculadora

---
+++ funcion.js
@@ -0,0 +1,23 @@
+function init(){
+    //Variables
+    var resultado = document.getElementById("resultado");
+    var borrar = document.getElementById("borrar");
+    var reset = document.getElementById("reset");
+    var on = document.getElementById("on");
+    var off = document.getElementById("off");
+    var division = document.getElementById("division");
+    var resta = document.getElementById("resta");
+    var multiplicacion = document.getElementById("multiplicacion");
+    var suma = document.getElementById("suma");
+    var dobleCERO = document.getElementById("dobleCero");
+    var cero = document.getElementById("cero");
+    var igual = document.getElementById("igual");
+    var uno = document.getElementById("uno");
+    var dos = document.getElementById("dos");
+    var tres = document.getElementById("tres");
+    var cuatro = document.getElementById("cuatro");
+    var cinco = document.getElementById("cinco");
+    var seis = document.getElementById("seis");
+    var siete = document.getElementById("siete");
+    var ocho = document.getElementById("ocho");
+    var nueve = document.getElementById("nueve");
